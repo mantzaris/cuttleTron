dbff17ee673fcf0da80270b9865c5811da5e4056
mantzaris <avmantzaris@gmail.com>
2023-12-25 12:39:24 -0500

    now the pitch slider color changes based on values

--- components/audioeffects/pitch/pitcheffect.js
+++ components/audioeffects/pitch/pitcheffect.js
@@ -1,14 +1,35 @@
-var labelText = "Select Pitch Value";
+var labelText = "pitch";
 var divArea = document.getElementById("audioeffects-controls");
-export var pitchValue = 0.8;
+//pitch is non altered with 1.0, min is 0.1 max 3 and moderate is 0.7 to 1.4
+export var pitchValue = 1.0;
 var minPitch = 0.1;
 var maxPitch = 3;
 export function populateEffectArea_Pitch() {
-    divArea.innerHTML = "<div id=\"sliderContainer\">\n                          <label id=\"pitchSliderLabel\" for=\"pitchSlider\">".concat(labelText, ":").concat(pitchValue, "</label>\n                          <div class=\"sliderWithValues\">\n                              <span class=\"minValue\">").concat(minPitch, "</span>\n                              <input type=\"range\" id=\"pitchSlider\" min=\"").concat(minPitch, "\" max=\"").concat(maxPitch, "\" step=\"0.01\" value=\"").concat(pitchValue, "\">\n                              <span class=\"maxValue\">").concat(maxPitch, "</span>\n                          </div>\n                      </div>\n                        ");
-    document.getElementById("pitchSlider").onchange = function (event) {
+    divArea.innerHTML = "<div id=\"sliderContainer\">\n                          <label id=\"pitchSliderLabel\" for=\"pitchSlider\">".concat(labelText, ":").concat(pitchValue, "</label>\n                          <div class=\"sliderWithValues\">\n                              <span class=\"minValue\">").concat(minPitch, "</span>\n                              <input type=\"range\" id=\"pitchSlider\" min=\"").concat(minPitch, "\" max=\"").concat(maxPitch, "\" step=\"0.1\" value=\"").concat(pitchValue, "\">\n                              <span class=\"maxValue\">").concat(maxPitch, "</span>\n                          </div>\n                      </div>\n                        ");
+    var pitchSlider = document.getElementById("pitchSlider");
+    pitchSlider.onchange = function (event) {
         var target = event.target;
         pitchValue = target.valueAsNumber; //parseFloat(target.value);
+        updateSlider(pitchSlider, pitchValue);
         document.getElementById("pitchSliderLabel").innerText = labelText + ": " + target.value;
     };
+    updateSlider(pitchSlider, pitchValue);
+}
+function updateSlider(slider, value) {
+    var color = sliderColor(value);
+    console.log(color);
+    slider.style.setProperty("--thumb-color", color);
+}
+function sliderColor(value) {
+    if (value === 1.0) {
+        console.log("blck");
+        return "black";
+    }
+    else if (value >= 0.7 && value <= 1.5) {
+        return "blue";
+    }
+    else {
+        return "red";
+    }
 }
 //# sourceMappingURL=pitcheffect.js.map
--- components/audioeffects/pitch/pitcheffect.ts
+++ components/audioeffects/pitch/pitcheffect.ts
@@ -1,6 +1,7 @@
-const labelText = "Select Pitch Value";
+const labelText = "pitch";
 let divArea = document.getElementById("audioeffects-controls");
-export let pitchValue: number = 0.8;
+//pitch is non altered with 1.0, min is 0.1 max 3 and moderate is 0.7 to 1.4
+export let pitchValue: number = 1.0;
 const minPitch = 0.1;
 const maxPitch = 3;
 
@@ -9,15 +10,38 @@ export function populateEffectArea_Pitch() {
                           <label id="pitchSliderLabel" for="pitchSlider">${labelText}:${pitchValue}</label>
                           <div class="sliderWithValues">
                               <span class="minValue">${minPitch}</span>
-                              <input type="range" id="pitchSlider" min="${minPitch}" max="${maxPitch}" step="0.01" value="${pitchValue}">
+                              <input type="range" id="pitchSlider" min="${minPitch}" max="${maxPitch}" step="0.1" value="${pitchValue}">
                               <span class="maxValue">${maxPitch}</span>
                           </div>
                       </div>
                         `;
 
-  document.getElementById("pitchSlider").onchange = (event) => {
+  const pitchSlider = document.getElementById("pitchSlider") as HTMLInputElement;
+
+  pitchSlider.onchange = (event) => {
     const target = event.target as HTMLInputElement;
     pitchValue = target.valueAsNumber; //parseFloat(target.value);
+    updateSlider(pitchSlider, pitchValue);
+
     document.getElementById("pitchSliderLabel").innerText = labelText + ": " + target.value;
   };
+
+  updateSlider(pitchSlider, pitchValue);
+}
+
+function updateSlider(slider: HTMLInputElement, value: number) {
+  const color = sliderColor(value);
+  console.log(color);
+  slider.style.setProperty("--thumb-color", color);
+}
+
+function sliderColor(value: number): string {
+  if (value === 1.0) {
+    console.log("blck");
+    return "black";
+  } else if (value >= 0.7 && value <= 1.5) {
+    return "blue";
+  } else {
+    return "red";
+  }
 }
